e077a36c2fe7af27a43634de1e93636f64ef1152
qadeveloper <info+dev@virginmobilecolombia.com>
2019-10-08 15:42:07 -0500

    fix entity profesor

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO profesor (name, asignature, creation_date) VALUES ('profesor 1', 'fisica', NOW());
+INSERT INTO profesor (name, asignature, creation_date) VALUES ('profesor 2', 'calculo', NOW());
+
